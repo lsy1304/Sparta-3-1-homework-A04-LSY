d8dfe14c551846b1261bfd45b1b12f946ff5ca81
lsy1304 <lsy130425@gmail.com>
2024-05-09 12:59:51 +0900

    Update TopDownMovement.cs
    카메라 이동 부분은 LateUpdate로 이동

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -29,6 +29,11 @@ public class TopDownMovement : MonoBehaviour
         ApplyLookDirection(lookDirection); // 주시 방향 확정
     }
 
+    private void LateUpdate()
+    {
+        followCamera.transform.position = transform.position + new Vector3(0f, 0f, -10f);
+    }
+
     private void Look(Vector2 direction) // 주시 관련 키 입력으로 변환한 실수값을 저장하는 메서드
     {
         lookDirection = direction.x;
@@ -50,6 +55,5 @@ public class TopDownMovement : MonoBehaviour
     {
         direction = direction * 5;
         movementRigidbody.velocity = direction;
-        followCamera.transform.position = transform.position + new Vector3(0f, 0f, -10f);
     }
 }
